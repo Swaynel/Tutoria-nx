16c5cc1ff758934d27631062b84f9c91a695d7a3
swaynel <sayaneezy254@gmail.com>
2025-09-25 19:52:25 +0300

    supabase key fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,12 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 
 // These are safe to access directly because they are public
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY! // Important!
-
-
-if (!supabaseUrl || !supabaseServiceKey) {
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY')
 }
 
 // Export a Supabase client instance
-export const supabase = createClient(supabaseUrl, supabaseServiceKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
